590fc0121d6e76ad8b3886503b41421aebd4ecdf
Lenko Ivanov <lenko.ivanov117@gmail.com>
2024-03-04 16:17:08 +0200

    feat: style the chat view component

--- client/src/Chat/Chat.tsx
+++ client/src/Chat/Chat.tsx
@@ -1,13 +1,39 @@
+import { useRef } from "react";
 import { Message } from "./Message/Message";
+import styles from "./Chat.module.scss";
 
 export const Chat = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
-    <div>
-      <Message
-        messageText="sfsadfdsafdsfdsfsdfsd fsdf sdf sdf sdf sdf sdfsdfsdf sdf sdf sdfsdf sdf"
-        sender="lenko"
-        sentDate={new Date()}
-      />
+    <div className={styles.chatContainer}>
+      <div className={styles.messagesWindow}>
+        <Message
+          messageText="sfsadfdsafdsfdsfsdfsd fsdf sdf sdf sdf sdf sdfsdfsdf sdf sdf sdfsdf sdf"
+          sender="lenko"
+          sentDate={new Date()}
+        />
+        <Message
+          messageText="sfsadfdsafdsfdsfsdfsd fsdf sdf sdf sdf sdf sdfsdfsdf sdf sdf sdfsdf sdf"
+          sender="rero ivanov"
+          sentDate={new Date()}
+        />
+        <Message
+          messageText="sfsadfdsafdsfdsfsdfsd fsdf sdf sdf sdf sdf sdfsdfsdf sdf sdf sdfsdf sdf"
+          sender="rero lelo"
+          sentDate={new Date()}
+        />
+        <Message
+          messageText="sfsadfdsafdsfdsfsdfsd fsdf sdf sdf sdf sdf sdfsdfsdf sdf sdf sdfsdf sdf"
+          sender="rero lelo"
+          sentDate={new Date()}
+        />
+      </div>
+      <div className={styles.sendMessage}>
+        <form>
+          <input ref={inputRef} />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 };
