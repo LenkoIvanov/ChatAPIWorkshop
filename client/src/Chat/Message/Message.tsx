7937a613355fb4bd9d2ebbdbe92d2742bb8fd040
Lenko Ivanov <lenko.ivanov117@gmail.com>
2024-03-04 16:00:43 +0200

    feat: generate a color for the bubble based on a randomizer function and a decoded jwt token + determine chat bubble position

--- client/src/Chat/Message/Message.tsx
+++ client/src/Chat/Message/Message.tsx
@@ -1,6 +1,10 @@
+import { jwtDecode } from "jwt-decode";
+import { currentUserColor, getRandomNeonColor } from "../../utils/chatColors";
+import { authTokenKey } from "../../utils/constants";
 import { ChatBubble } from "../ChatBubble/ChatBubble";
 import { MessageBox } from "../MessageBox/MessageBox";
 import styles from "./Message.module.scss";
+import { JWTToken } from "../../utils/interfaces";
 
 interface MessageProps {
   messageText: string;
@@ -10,10 +14,28 @@ interface MessageProps {
 
 export const Message = (props: MessageProps) => {
   const { messageText, sender, sentDate } = props;
+
+  const isSenderLoggedIn = (): boolean => {
+    const storedToken = sessionStorage.getItem(authTokenKey);
+    if (storedToken) {
+      const decodedToken = jwtDecode<JWTToken>(storedToken);
+      return decodedToken.username === sender ? true : false;
+    }
+    return false; //fallback
+  };
+
+  const getChatBubbleColor = (): string => {
+    return isSenderLoggedIn() ? currentUserColor : getRandomNeonColor();
+  };
+
   return (
-    <div className={styles.message}>
+    <div
+      className={`${styles.message} ${
+        isSenderLoggedIn() ? "" : styles.differentSender
+      }`}
+    >
       <MessageBox messageTxt={messageText} sentDate={sentDate} />
-      <ChatBubble color="red" name={sender} />
+      <ChatBubble color={getChatBubbleColor()} name={sender} />
     </div>
   );
 };
---
+++ client/src/utils/interfaces.ts
@@ -0,0 +1,5 @@
+export interface JWTToken {
+  username: string;
+  exp: number;
+  iat: number;
+}
